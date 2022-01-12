287a8c0d213d825590dab6ec3ffef0d1fdad8145
Serendipity <jacksparrow414@163.com>
2022-01-12 09:03:10 +0800

    add comment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component} from "react";
-import { hot } from "react-hot-loader";
+import React from "react";
 import "./App.css";
 
-class App extends Component{
-  render(){
+export default function App() {
+  // return 为什么要加()?
+  // 这是因为 JavaScript 会自动给行末添加分号。如果 return 后面换行不加括号就会变成 return;
+  // 解答地址：
+  // https://blog.csdn.net/weixin_43459866/article/details/117279110
+  // https://segmentfault.com/q/1010000009504727###
     return(
       <div className="App">
         <h1> Hello, World! </h1>
       </div>
     );
-  }
 }
-
-export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+//  React 官方文档 说明了DOM节点为 root的原因https://zh-hans.reactjs.org/docs/rendering-elements.html
 ReactDOM.render(<App />, document.getElementById("root"));
